1d523289a39733b351576d8f0d7c5f988ea38c15
matineh <meybpoosh@wattlearn.com>
2021-01-05 14:43:03 -0500

    propagation function added to utilities

--- utils.py
+++ utils.py
@@ -26,3 +26,45 @@ def display_results(imgs, phases, recons):
             for i in range(img.shape[-1]):
                 axs[i].imshow(img[:, :, i], cmap='gray')
                 axs[i + img.shape[-1]].imshow(recon[:, :, i], cmap='gray')
+
+def get_propagate(data, model):
+    shape = data['shape']
+    zs = [-0.005*x for x in np.arange(1, (shape[-1]-1)//2+1)][::-1] + [0.005*x for x in np.arange(1, (shape[-1]-1)//2+1)]
+    lambda_ = model['wavelength']
+    ps = model['pixel_size']
+
+    def __get_H(zs, shape, lambda_, ps):
+        Hs = []
+        for z in zs:
+            x, y = np.meshgrid(np.linspace(-shape[1] // 2 + 1, shape[1] // 2, shape[1]),
+                               np.linspace(-shape[0] // 2 + 1, shape[0] // 2, shape[0]))
+            fx = x / ps / shape[0]
+            fy = y / ps / shape[1]
+            exp = np.exp(-1j * np.pi * lambda_ * z * (fx ** 2 + fy ** 2))
+            Hs.append(exp.astype(np.complex64))
+        return Hs
+
+    def __prop__(cf_slm, H=None, center=False):
+        if not center:
+            H = tf.broadcast_to(tf.expand_dims(H, axis=0), tf.shape(cf_slm))
+            cf_slm *= tf.signal.fftshift(H, axes=[1, 2])
+        fft = tf.signal.ifftshift(tf.signal.fft2d(tf.signal.fftshift(cf_slm, axes=[1, 2])), axes=[1, 2])
+        img = tf.cast(tf.expand_dims(tf.abs(tf.pow(fft, 2)), axis=-1), dtype=tf.dtypes.float32)
+        return img
+
+    def __phi_slm(phi_slm):
+        i_phi_slm = tf.dtypes.complex(np.float32(0.), tf.squeeze(phi_slm, axis=-1))
+        return tf.math.exp(i_phi_slm)
+
+    Hs = __get_H(self.zs, self.shape, self.wavelength, self.ps)
+
+    def propagate(phi_slm):
+        frames = []
+        cf_slm = __phi_slm(phi_slm)
+        for H, z in zip(Hs, self.zs):
+            frames.append(__prop__(cf_slm, tf.keras.backend.constant(H, dtype=tf.complex64)))
+
+        frames.insert(self.shape[-1] // 2, __prop__(cf_slm, center=True))
+
+        return tf.concat(values=frames, axis=-1)
+    return propagate
