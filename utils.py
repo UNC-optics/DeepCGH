25edbdd81eddb5b849a0ac5a1d4efa21c701ca3b
Hoss Eybposh <m.h.eybpoosh@gmail.com>
2021-02-12 12:53:41 -0500

    Mix utils with novocgh2D

--- utils.py
+++ utils.py
@@ -150,3 +150,77 @@ def get_propagate(data, model):
 def accuracy(y_true, y_pred):
     denom = tf.sqrt(tf.reduce_sum(tf.pow(y_pred, 2), axis=[1, 2, 3])*tf.reduce_sum(tf.pow(y_true, 2), axis=[1, 2, 3]))
     return tf.reduce_mean((tf.reduce_sum(y_pred * y_true, axis=[1, 2, 3])+1)/(denom+1), axis = 0)
+
+#%%
+@tf.function
+def __normalize_minmax(img):
+    img -= tf.reduce_min(tf.cast(img, tf.float32), axis=[0, 1], keepdims=True)
+    img /= tf.reduce_max(img, axis=[0, 1], keepdims=True)
+    return img
+
+@tf.function
+def __gs(img):
+    rand_phi = tf.random.uniform(img.shape)
+    img = __normalize_minmax(img)
+    img_cf = tf.complex(img, 0.) * tf.math.exp(tf.complex(0., rand_phi))
+    slm_cf = tf.signal.ifft2d(tf.signal.ifftshift(img_cf))
+    slm_phi = tf.math.angle(slm_cf)
+    return slm_phi
+
+
+def __accuracy(y_true, y_pred):
+    denom = tf.sqrt(tf.reduce_sum(tf.pow(y_pred, 2), axis = [0, 1])*tf.reduce_sum(tf.pow(y_true, 2), axis = [0, 1]))
+    return 1 - (tf.reduce_sum(y_pred * y_true, axis = [0, 1])+1)/(denom+1)
+
+
+def novocgh2D(img, Ks, lr = 0.01):
+    slms = []
+    amps = []
+    phi = __gs(img)
+    phi_slm = tf.Variable(phi)
+    opt = tf.keras.optimizers.Adam(learning_rate = lr)
+    img = tf.convert_to_tensor(img)
+
+    def loss_(phi_slm):
+        slm_cf = tf.math.exp(tf.complex(0., phi_slm))
+        img_cf = tf.signal.fftshift(tf.signal.fft2d(slm_cf))
+        return tf.math.abs(img_cf)
+
+    def loss():
+        slm_cf = tf.math.exp(tf.complex(0., phi_slm))
+        img_cf = tf.signal.fftshift(tf.signal.fft2d(slm_cf))
+        amp = tf.math.abs(img_cf)
+        return __accuracy(tf.square(img), tf.square(amp))
+
+    for i in range(Ks[-1]+1):
+        opt.minimize(loss, var_list=[phi_slm])
+        if i in Ks:
+            amps.append(loss_(phi_slm).numpy())
+            slms.append(phi_slm.numpy())
+    return slms, amps
+
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
