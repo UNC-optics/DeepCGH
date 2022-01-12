H Scaling fixed. DeepCGH module now accepts external H matrix too.

deepcgh.py
@@ -407,9 +407,27 @@ class DeepCGH(object):
         self.token = model_params['token']
         self.shuffle = model_params['shuffle']
         self.max_steps = model_params['max_steps']
-        self.f = model_params['fourier_lens_f']
+        self.f = model_params['focal_point']
+        try:
+            self.Hs = model_params['HMatrix']
+        except:
+            self.Hs = self.__get_H(self.zs, self.shape, self.wavelength, self.ps, self.f)
         
-        
+    
+    def __get_H(self, zs, shape, lambda_, ps, f):
+        psx = np.abs(f*lambda_/(shape[1]*ps))
+        psy = np.abs(f*lambda_/(shape[0]*ps))
+        Hs = []
+        for z in zs:
+            x, y = np.meshgrid(np.linspace(-shape[1]//2+1, shape[1]//2, shape[1]),
+                               np.linspace(-shape[0]//2+1, shape[0]//2, shape[0]))
+            fx = x/psx/shape[1]
+            fy = y/psy/shape[0]
+            exp = np.exp(-1j * np.pi * lambda_ * z * (fx**2 + fy**2))
+            Hs.append(exp.astype(np.complex64))
+        return Hs
+    
+    
     def __start_thread(self):
         self.prediction_thread = Thread(target=self.__predict_from_queue, daemon=True)
         self.prediction_thread.start()
@@ -598,20 +616,6 @@ class DeepCGH(object):
             return x1
         
         
-        def __get_fH(zs, shape, lambda_, ps, f):
-            psx = np.abs(f*lambda_/(shape[0]*ps))
-            psy = np.abs(f*lambda_/(shape[1]*ps))
-            Hs = []
-            for z in zs:
-                x, y = np.meshgrid(np.linspace(-shape[1]//2+1, shape[1]//2, shape[1]),
-                                   np.linspace(-shape[0]//2+1, shape[0]//2, shape[0]))
-                fx = x/psx/shape[1]
-                fy = y/psy/shape[0]
-                exp = np.exp(-1j * np.pi * lambda_ * z * (fx**2 + fy**2))
-                Hs.append(exp.astype(np.complex64))
-            return Hs
-        
-        
         def __unet():
             n_kernels = self.n_kernels
             inp = Input(shape=self.shape, name='target')
@@ -675,9 +679,6 @@ class DeepCGH(object):
         def __phi_slm(phi_slm):
             i_phi_slm = tf.dtypes.complex(np.float32(0.), tf.squeeze(phi_slm, axis=-1))
             return tf.math.exp(i_phi_slm)
-        
-        
-        self.Hs = __get_fH(self.zs, self.shape, self.wavelength, self.ps, self.f)
         
         
         def __big_loss(y_true, phi_slm):

demo_infinity_loop.py
@@ -18,7 +18,7 @@ coordinates = False
 
 data = {
         'path' : 'DeepCGH_Datasets/Disks',
-        'shape' : (512, 512, 3),
+        'shape' : (1152, 1920, 5),
         # 'shape' : (slm.height, slm.width, 3),
         'object_type' : 'Disk',
         'object_size' : 10,
@@ -26,8 +26,8 @@ data = {
         'intensity' : [0.2, 1],
         'normalize' : True,
         'centralized' : False,
-        'N' : 20000,
-        'train_ratio' : 19000/20000,
+        'N' : 5000,
+        'train_ratio' : 4000/5000,
         'compression' : 'GZIP',
         'name' : 'target',
         }
@@ -38,6 +38,7 @@ model = {
         'int_factor':32,
         'n_kernels':[ 64, 128, 256],
         'plane_distance':0.005,
+        'focal_point':0.2,
         'wavelength':1e-6,
         'pixel_size':0.000015,
         'input_name':'target',
@@ -47,8 +48,7 @@ model = {
         'epochs' : 10,
         'token' : '64',
         'shuffle' : 8,
-        'max_steps' : 4000,
-        'fourier_lens_f' : 0.2
+        'max_steps' : 4000
         }
 
 
@@ -78,6 +78,7 @@ while(True):
 
 
 #%% This is a sample test. You can generate a random image and get the results
+propagate = get_propagate(data, model)
 image = dset.get_randSample()[np.newaxis,...]
 # making inference is as simple as calling the get_hologram method
 phase = dcgh.get_hologram(image)

utils.py
@@ -87,7 +87,7 @@ def display_results(imgs, phases, recons, t):
     assert imgs.ndim == 4 and phases.ndim == 4 and recons.ndim == 4, "Dimensions don't match"
     for img, phase, recon in zip(imgs, phases, recons):
         if img.shape[-1] == 1:
-            fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True, sharex=True)
+            fig, axs = plt.subplots(1, 3, figsize=(9, 5), sharey=True, sharex=True)
             axs[0].imshow(np.squeeze(img), cmap='gray')
             axs[0].set_title('Target')
             axs[1].imshow(np.squeeze(phase), cmap='gray')
@@ -95,7 +95,7 @@ def display_results(imgs, phases, recons, t):
             axs[2].imshow(np.squeeze(recon), cmap='gray')
             axs[2].set_title('Simulation')
         else:
-            fig, axs = plt.subplots(2, img.shape[-1] + 1, figsize = (3 * (img.shape[-1] + 1), 6), sharey = True, sharex = True)
+            fig, axs = plt.subplots(2, img.shape[-1] + 1, figsize = (15 * (img.shape[-1] + 1), 30), sharey = True, sharex = True)
             axs[0, -1].imshow(np.squeeze(phase))
             axs[0, -1].set_title('SLM Phase')
             for i in range(img.shape[-1]):
@@ -104,13 +104,15 @@ def display_results(imgs, phases, recons, t):
                 axs[1, i].imshow(recon[:, :, i], cmap='gray')
                 axs[1, i].set_title('Reconstructed')
         fig.suptitle('Inference time was {:.2f}ms'.format(t*1000), fontsize=16)
+        fig.savefig('deepcghresults.png')
 
-def get_propagate(shape, wavelength, pixel_size, Zs):
+def get_propagate(data, model):
+    shape = data['shape']
     zs = [-0.005*x for x in np.arange(1, (shape[-1]-1)//2+1)][::-1] + [0.005*x for x in np.arange(1, (shape[-1]-1)//2+1)]
     lambda_ = model['wavelength']
     ps = model['pixel_size']
 
-    def __get_fH(zs, shape, lambda_, ps):
+    def __get_H(zs, shape, lambda_, ps):
         Hs = []
         for z in zs:
             x, y = np.meshgrid(np.linspace(-shape[1] // 2 + 1, shape[1] // 2, shape[1]),
@@ -132,13 +134,13 @@ def get_propagate(shape, wavelength, pixel_size, Zs):
     def __phi_slm(phi_slm):
         i_phi_slm = tf.dtypes.complex(np.float32(0.), tf.squeeze(phi_slm, axis=-1))
         return tf.math.exp(i_phi_slm)
-    
-    Hs = __get_fH(zs, shape, lambda_, ps)
+    Hs = model['HMatrix']
+    # Hs = __get_H(zs, shape, lambda_, ps)
 
     def propagate(phi_slm):
         frames = []
         cf_slm = __phi_slm(phi_slm)
-        for H in Hs:
+        for H, z in zip(Hs, zs):
             frames.append(__prop__(cf_slm, tf.keras.backend.constant(H, dtype=tf.complex64)))
 
         frames.insert(shape[-1] // 2, __prop__(cf_slm, center=True))